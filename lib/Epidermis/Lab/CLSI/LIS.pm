9ea3c424b9a867c5ba838ccf0fa962664ef40a2a
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-08-16 00:47:47 -0500

    Add README

--- lib/Epidermis/Lab/CLSI/LIS.pm
+++ lib/Epidermis/Lab/CLSI/LIS.pm
@@ -14,14 +14,12 @@ standard protocol as specified by
 
 =over 4
 
-=item
-* L<CLSI LIS01-A2
+=item * L<CLSI LIS01-A2
 |https://clsi.org/standards/products/automation-and-informatics/documents/lis01/>
 (formerly ASTM E1381)
 Specification for Low-Level Protocol to Transfer Messages Between Clinical Laboratory Instruments and Computer Systems, 2nd Edition.
 
-=item
-* L<CLSI LIS02-A2
+=item * L<CLSI LIS02-A2
 |https://clsi.org/standards/products/automation-and-informatics/documents/lis02/>
 (formerly ASTM E1394)
 Specification for Transferring Information Between Clinical Laboratory Instruments and Information Systems, 2nd Edition.
