a237c2ded2bd919ad1b22e3034d282351929ecf1
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-09-15 00:14:46 -0500

    Add skeletons for LIS01A2 and LIS02A2

---
+++ lib/Epidermis/Lab/CLSI/LIS/LIS01A2.pm
@@ -0,0 +1,9 @@
+use Modern::Perl;
+package Epidermis::Lab::CLSI::LIS::LIS01A2;
+# ABSTRACT: Low-level protocol
+
+use Epidermis::Lab::CLSI::LIS::LIS01A2::Session;
+use Epidermis::Lab::CLSI::LIS::LIS01A2::Message;
+use Epidermis::Lab::CLSI::LIS::LIS01A2::Frame;
+
+1;
