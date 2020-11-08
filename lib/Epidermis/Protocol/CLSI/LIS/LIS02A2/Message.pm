cee2b1ee00541e83cc6d4e7ad628e8298df3bbe3
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-11-08 00:13:25 -0600

    Check for invalid messages
    - Check for records after MessageTerminator
    - Check record levels and sequences.

--- lib/Epidermis/Protocol/CLSI/LIS/LIS02A2/Message.pm
+++ lib/Epidermis/Protocol/CLSI/LIS/LIS02A2/Message.pm
@@ -7,6 +7,14 @@ use MooX::HandlesVia;
 use Types::Standard qw(ArrayRef ConsumerOf);
 
 use Epidermis::Protocol::CLSI::LIS::Constants qw(RECORD_SEP);
+use aliased 'Epidermis::Protocol::CLSI::LIS::LIS02A2::Record::MessageHeader';
+use aliased 'Epidermis::Protocol::CLSI::LIS::LIS02A2::Record::MessageTerminator';
+
+use failures qw(
+	LIS02A2::Message::InvalidRecordNumberSequence
+	LIS02A2::Message::InvalidRecordLevel
+	LIS02A2::Message::RecordAfterEndRecord
+);
 
 has codec => (
 	is => 'rw',
@@ -24,6 +32,12 @@ has records => (
 		number_of_records => 'count',
 		_push_record => 'push',
 	},
+);
+
+has _records_stack => (
+	is => 'ro',
+	isa => ArrayRef[ConsumerOf['Epidermis::Protocol::CLSI::LIS::LIS02A2::Meta::Record']],
+	default => sub { [] },
 );
 
 sub _set_codec_from_record_text {
@@ -45,9 +59,54 @@ sub add_record_text {
 	$self->add_record( $data );
 }
 
+sub _check_record_increment_sequence {
+	my ($self, $first, $second) = @_;
+	$first->sequence->{data} + 1 == $second->sequence->{data};
+}
+
+sub _handle_stack {
+	my ($self, $stack, $records, $record, $record_idx) = @_;
+	if( @$stack == 0 ) {
+		push @$stack, $record_idx;
+		return;
+	}
+
+	if( $records->[ $stack->[-1] ]->type_id eq $record->type_id ) {
+		my $last_idx = pop @$stack;
+		if( ! $self->_check_record_increment_sequence($records->[ $last_idx ], $record) ) {
+			failure::LIS02A2::Message::InvalidRecordNumberSequence->throw;
+		}
+
+		push @$stack, $record_idx;
+		return;
+	} else {
+		if( ! defined $record->_level || $record->_level == @$stack ) {
+			push @$stack, $record_idx;
+			return;
+		} elsif( $record->_level < @$stack ) {
+			while( $record->_level < @$stack ) {
+				pop @$stack;
+			}
+			$self->_handle_stack( $stack, $records, $record, $record_idx );
+		} else {
+			failure::LIS02A2::Message::InvalidRecordLevel->throw;
+		}
+	}
+}
+
 sub add_record {
 	my ($self, $record) = @_;
-	# TODO check sequence and level
+	failure::LIS02A2::Message::RecordAfterEndRecord->throw
+		if( $self->is_complete );
+
+	if( $self->is_empty
+		&& $record->type_id ne MessageHeader->type_id ) {
+		failure::LIS02A2::Message::InvalidRecordLevel->throw;
+	}
+
+	# Check sequence and level
+	$self->_handle_stack( $self->_records_stack, $self->records,
+		$record, $self->number_of_records );
 	$self->_push_record( $record );
 }
 
@@ -105,6 +164,11 @@ sub create_message {
 	}
 
 	$message;
+}
+
+sub is_complete {
+	my ($self) = @_;
+	! $self->is_empty && $self->records->[-1]->type_id eq MessageTerminator->type_id;
 }
 
 1;
