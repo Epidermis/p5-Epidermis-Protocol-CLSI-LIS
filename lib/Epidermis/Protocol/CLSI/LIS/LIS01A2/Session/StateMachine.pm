e50d3d740c82305e090972a5f8811842a8d13df0
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-02-01 21:28:02 -0600

    Add state machine map

--- lib/Epidermis/Protocol/CLSI/LIS/LIS01A2/Session/StateMachine.pm
+++ lib/Epidermis/Protocol/CLSI/LIS/LIS01A2/Session/StateMachine.pm
@@ -3,62 +3,81 @@ package Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::StateMachine;
 
 use Moo;
 use MooX::Enumeration;
+use Sub::Trigger::Lock;
 
 use boolean;
 use Const::Fast;
 
-use Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::Constants qw(:enum_device :enum_state);
+use Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::Constants
+	qw(:enum_device :enum_state :enum_event :enum_action);
 
-	# - Await for data to send => STATE_S_ESTABLISH_SEND_DATA
-	# - Receive <ENQ>          => STATE_R_AWAKE
+has _state_map => ( is => 'ro', default => sub { +{} } );
 
-no strict "subs"; ## no critic: 'RequireUseStrict'
+sub _t {
+	my ($self, %args) = @_;
 
-const our $STATE_MAP => {
-	STATE_N_IDLE ,=> {
-		STATE_S_ESTABLISH_SEND_DATA ,=> {
-			on => { code => \&_has_data_to_send, },
-			do => { code => \&_set_device_to_sender, },
-		},
-		STATE_R_AWAKE ,=> {
-			on => { code => \&_on_receive_enq, },
-			do => { code => \&_set_device_to_receiver, }
-		},
-	},
+	my $from = $args{from} or die "Transition requires 'from' argument";
+	my $to = $args{to} or die "Transition requires 'to' argument";
+	my $event = $args{event} or die "Transition requires 'event' argument";
+	my $action = $args{action} or die "Transition requires 'action' argument";
 
-	STATE_R_AWAKE ,=> {
-		STATE_N_IDLE ,=> {
-			on => { code => \&_on_is_busy, },
-		},
-		STATE_R_WAITING ,=> { },
-	},
-	STATE_R_WAITING ,=> {
-		STATE_R_FRAME_RECEIVED ,=> {},
-		STATE_N_IDLE ,=> {},
-	},
-	STATE_R_FRAME_RECEIVED ,=> {
-	},
-	STATE_R_SEND_DATA ,=> {
-	},
+	die "Already have transition from $from to $to"
+		if exists $self->_state_map->{ $from }{ $to };
 
-	STATE_S_ESTABLISH_SEND_DATA ,=> {
-	},
-	STATE_S_ESTABLISH_CONTENTION_BUSY ,=> {
-	},
-	STATE_S_ESTABLISH_WAITING ,=> {
-	},
+	$self->_state_map->{ $from }{ $to } = {
+		event => $event,
+		action => ref $action ? $action : [ $action ],
+	};
+}
 
-	STATE_S_TRANSFER_SETUP_NEXT_FRAME ,=> {
-	},
-	STATE_S_TRANSFER_FRAME_READY ,=> {
-	},
-	STATE_S_TRANSFER_WAITING ,=> {
-	},
-	STATE_S_TRANSFER_SETUP_OLD_FRAME ,=> {
-	},
-	STATE_S_TRANSFER_INTERRUPT ,=> {
-	},
-};
+sub BUILD {
+	my ($self, $args) = @_;
+
+	# Neutral Device
+	$self->_t( from => STATE_N_IDLE, to => STATE_S_ESTABLISH_SEND_DATA, event => EV_HAS_DATA_TO_SEND, action => ACTION_SET_DEVICE_TO_SENDER  );
+	$self->_t( from => STATE_N_IDLE, to => STATE_R_AWAKE, event => EV_RECEIVE_ENQ, action => ACTION_SET_DEVICE_TO_RECEIVER );
+
+	# Receiver Device
+	$self->_t( from => STATE_R_AWAKE, to => STATE_N_IDLE, event => EV_BUSY, action => [ ACTION_SET_DEVICE_TO_NEUTRAL, ACTION_SEND_NAK ] );
+	$self->_t( from => STATE_R_AWAKE, to => STATE_R_WAITING, event => EV_NOT_BUSY, action => [ ACTION_SET_INITIAL_FN, ACTION_SEND_ACK, ACTION_RESET_RECEIVER_TIMER ] );
+
+	$self->_t( from => STATE_R_WAITING, to => STATE_R_FRAME_RECEIVED, event => EV_GET_FRAME, action => ACTION_NOP );
+	$self->_t( from => STATE_R_WAITING, to => STATE_N_IDLE, event => EV_RECEIVE_EOT_OR_TIME_OUT, action => ACTION_SET_DEVICE_TO_NEUTRAL );
+
+	$self->_t( from => STATE_R_FRAME_RECEIVED, to => STATE_R_GOOD_FRAME, event => EV_GOOD_FRAME, action => ACTION_NOP );
+	$self->_t( from => STATE_R_FRAME_RECEIVED, to => STATE_R_WAITING, event => EV_BAD_FRAME, action => [ ACTION_RESET_RECEIVER_TIMER, ACTION_SEND_NAK ] );
+
+	$self->_t( from => STATE_R_GOOD_FRAME, to => STATE_R_WAITING, event => EV_NOT_HAS_DATA_TO_SEND, action => [ ACTION_INCREMENT_FN, ACTION_RESET_RECEIVER_TIMER, ACTION_SEND_ACK ] );
+	$self->_t( from => STATE_R_GOOD_FRAME, to => STATE_R_SEND_DATA, event => EV_HAS_DATA_TO_SEND, action => ACTION_NOP );
+
+	$self->_t( from => STATE_R_SEND_DATA, to => STATE_R_WAITING, event => EV_ANY, action => [ ACTION_INCREMENT_FN, ACTION_RESET_RECEIVER_TIMER, ACTION_SEND_EOT ] );
+
+	# Sending Device
+	$self->_t( from => STATE_S_ESTABLISH_SEND_DATA, to => STATE_S_ESTABLISH_WAITING, event => EV_ESTABLISHMENT_TIMERS_TIMED_OUT, action => [ ACTION_SET_INITIAL_FN, ACTION_SEND_ENQ, ACTION_RESET_SENDER_TIMER ] );
+	$self->_t( from => STATE_S_ESTABLISH_SEND_DATA, to => STATE_S_ESTABLISH_CONTENTION_BUSY, event => EV_ESTABLISHMENT_TIMERS_RUNNING, action => ACTION_NOP );
+
+	$self->_t( from => STATE_S_ESTABLISH_CONTENTION_BUSY, to => STATE_N_IDLE, event => EV_ANY, action => ACTION_SET_DEVICE_TO_NEUTRAL );
+
+	$self->_t( from => STATE_S_ESTABLISH_WAITING, to => STATE_S_TRANSFER_SETUP_NEXT_FRAME, event => EV_RECEIVE_ACK, action => ACTION_RESET_RETRY_COUNT );
+	$self->_t( from => STATE_S_ESTABLISH_WAITING, to => STATE_S_ESTABLISH_CONTENTION_BUSY, event => EV_RECEIVE_ENQ_OR_NAK, action => ACTION_RESET_CONTENTION_BUSY_TIMER );
+	$self->_t( from => STATE_S_ESTABLISH_WAITING, to => STATE_N_IDLE, event => EV_TIMED_OUT, action => [ ACTION_SEND_EOT, ACTION_SET_DEVICE_TO_NEUTRAL ] );
+
+	$self->_t( from => STATE_S_TRANSFER_SETUP_NEXT_FRAME, to => STATE_N_IDLE, event => EV_NOT_HAS_DATA_TO_SEND, action => [ ACTION_SEND_EOT, ACTION_SET_DEVICE_TO_NEUTRAL ] );
+	$self->_t( from => STATE_S_TRANSFER_SETUP_NEXT_FRAME, to => STATE_S_TRANSFER_FRAME_READY, event => EV_HAS_DATA_TO_SEND, action => ACTION_SETUP_NEXT_FRAME );
+
+	$self->_t( from => STATE_S_TRANSFER_FRAME_READY, to => STATE_S_TRANSFER_WAITING, event => EV_ANY, action => [ ACTION_SEND_FRAME, ACTION_RESET_SENDER_TIMER ] );
+
+	$self->_t( from => STATE_S_TRANSFER_WAITING, to => STATE_S_TRANSFER_SETUP_OLD_FRAME, event => EV_RECEIVE_NAK_OR_FAIL, action => ACTION_INCREMENT_RETRY_COUNT );
+	$self->_t( from => STATE_S_TRANSFER_WAITING, to => STATE_S_TRANSFER_SETUP_NEXT_FRAME, event => EV_RECEIVE_ACK, action => [ ACTION_RESET_RETRY_COUNT, ACTION_INCREMENT_FN ] );
+	$self->_t( from => STATE_S_TRANSFER_WAITING, to => STATE_S_TRANSFER_INTERRUPT, event => EV_RECEIVE_EOT, action => [ ACTION_RESET_SENDER_TIMER, ACTION_REQUEST_INTERRUPT ] );
+	$self->_t( from => STATE_S_TRANSFER_WAITING, to => STATE_N_IDLE, event => EV_TIMED_OUT, action => [ ACTION_SEND_EOT, ACTION_SET_DEVICE_TO_NEUTRAL ] );
+
+	$self->_t( from => STATE_S_TRANSFER_SETUP_OLD_FRAME, to => STATE_S_TRANSFER_FRAME_READY, event => EV_CAN_RETRY, action =>  ACTION_SETUP_OLD_FRAME);
+	$self->_t( from => STATE_S_TRANSFER_SETUP_OLD_FRAME, to => STATE_N_IDLE, event => EV_NO_CAN_RETRY, action =>  [ ACTION_SEND_EOT, ACTION_SET_DEVICE_TO_NEUTRAL ] );
+
+	$self->_t( from => STATE_S_TRANSFER_INTERRUPT, to => STATE_S_TRANSFER_SETUP_NEXT_FRAME, event => EV_INTERRUPT_IGNORE, action => [ ACTION_RESET_RETRY_COUNT, ACTION_INCREMENT_FN ]);
+	$self->_t( from => STATE_S_TRANSFER_INTERRUPT, to => STATE_N_IDLE, event => EV_INTERRUPT_ACCEPT_OR_TIME_OUT, action => [ ACTION_SEND_EOT, ACTION_SET_DEVICE_TO_NEUTRAL ]);
+}
 
 sub reset {
 	my ($self, $context) = @_;
