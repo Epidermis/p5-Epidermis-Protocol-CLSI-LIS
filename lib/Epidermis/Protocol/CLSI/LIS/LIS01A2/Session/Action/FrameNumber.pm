c914fee782b029acedadb6958aa2a1365ec69006
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-02-09 11:40:11 -0600

    Implement FrameNumber session actions

--- lib/Epidermis/Protocol/CLSI/LIS/LIS01A2/Session/Action/FrameNumber.pm
+++ lib/Epidermis/Protocol/CLSI/LIS/LIS01A2/Session/Action/FrameNumber.pm
@@ -1,10 +1,30 @@
 package Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::Action::FrameNumber;
 # ABSTRACT: Frame number actions
 
-use Moo::Role;
+use Mu::Role;
+use Future::AsyncAwait;
+use MooX::Should;
 
-sub do_set_initial_fn { ... }
+use Epidermis::Protocol::CLSI::LIS::Types qw(FrameNumber);
+use Epidermis::Protocol::CLSI::LIS::Constants qw(LIS01A2_FIRST_FRAME_NUMBER);
 
-sub do_increment_fn { ... }
+has _frame_number => (
+	is => 'rw',
+	should => FrameNumber,
+);
+
+sub _increment_frame_number {
+	my ($self) = @_;
+	my $fn = $self->_frame_number;
+	$self->_frame_number( ( $fn + 1 ) % 8 );
+}
+
+async sub do_set_initial_fn {
+	$_[0]->_frame_number( LIS01A2_FIRST_FRAME_NUMBER );
+}
+
+async sub do_increment_fn {
+	$_[0]->_increment_frame_number;
+}
 
 1;
