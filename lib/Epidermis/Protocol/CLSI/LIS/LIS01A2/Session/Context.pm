90e478e26c55b0bec0903394631976b1d9959a6a
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-03-03 23:32:13 -0600

    Remove ::Session::Context

--- lib/Epidermis/Protocol/CLSI/LIS/LIS01A2/Session/Context.pm
+++
@@ -1,8 +0,0 @@
-package Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::Context;
-# ABSTRACT: Context for a session
-
-use Moo;
-
-use Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::Constants qw( :enum_system :enum_device :enum_state );
-
-1;
