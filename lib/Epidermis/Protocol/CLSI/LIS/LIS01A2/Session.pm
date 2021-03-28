482635529883933d89ca503b11d9f6fb1d14c6c6
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-03-28 05:39:07 -0500

    Move state machine step method to role

--- lib/Epidermis/Protocol/CLSI/LIS/LIS01A2/Session.pm
+++ lib/Epidermis/Protocol/CLSI/LIS/LIS01A2/Session.pm
@@ -9,7 +9,6 @@ use MooX::Role::Logger (); # declare dependency (role)
 use overload
 	'""' => \&TO_STRING;
 
-use Data::Dumper;
 use Data::Hexdumper ();
 
 use Types::Standard qw(ArrayRef InstanceOf);
@@ -88,52 +87,6 @@ async sub _send_data {
 	await Future::IO->syswrite_exactly( $self->connection->handle , $data );
 }
 
-async sub step {
-	my ($self) = @_;
-	my $events = $self->state_machine->events_for_state( $self->session_state );
-	do {
-		local $Data::Dumper::Terse = 1;
-		local $Data::Dumper::Indent = 0;
-		$self->_logger->debug( "State @{[ $self->session_state ]}: Events " . Dumper([ sort { $a cmp $b } @$events ]) )
-	} if $self->_logger->is_debug;
-	my @events_cb = @{ $self->_event_dispatch_table }{ @$events };
-
-	my $transition_event = await Future->needs_any( map {
-		my $idx = $_;
-		my $f = $events_cb[$idx]->($self)
-			->transform( done => sub { $events->[$idx] } )
-			->set_label( $events->[$idx] )
-	} 0..@events_cb-1 )->set_label("$self : events");
-
-	my $from = $self->session_state;
-	my $transition_data = $self->state_machine->process_event(
-		$self,
-		$transition_event
-	);
-
-	do {
-		$self->_logger->debug( "Transition: [ @{[ $from ]} ] -- @{[ $transition_event ]} --> [ @{[ $transition_data->{to} ]} ]" )
-	} if $self->_logger->is_debug;
-
-	$self->session_state( $transition_data->{to} );
-
-	my @actions = @{ $transition_data->{action} };
-	my $actions_done = Future->needs_all( map {
-		my $action = $_;
-		my $f = $self->_action_dispatch_table->{$action}->($self)
-			->set_label($action)
-	} @actions)->set_label( 'actions' );
-
-	await $actions_done->followed_by( sub { $self->_reset_after_step; Future->done } );
-
-	return {
-		from => $from,
-		transition => $transition_event,
-		to => $transition_data->{to}
-	};
-}
-
-sub _reset_after_step { }
 
 sub TO_STRING {
 	my ($self) = @_;
--- lib/Epidermis/Protocol/CLSI/LIS/LIS01A2/Session/Role/StateMachine.pm
+++ lib/Epidermis/Protocol/CLSI/LIS/LIS01A2/Session/Role/StateMachine.pm
@@ -4,12 +4,16 @@ package Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::Role::StateMachine;
 use Mu::Role;
 use MooX::Enumeration;
 
+use Data::Dumper;
+
 use Types::Standard qw(Enum);
 
 use aliased 'Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::StateMachine';
 
 use Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::Constants
 	qw(:enum_state);
+
+use Future::AsyncAwait;
 
 has state_machine => (
 	is => 'ro',
@@ -27,5 +31,52 @@ sub STATE_TO_STRING {
 	my ($self) = @_;
 	"[ State: <@{[ $self->session_state ]}> ]";
 }
+
+async sub step {
+	my ($self) = @_;
+	my $events = $self->state_machine->events_for_state( $self->session_state );
+	do {
+		local $Data::Dumper::Terse = 1;
+		local $Data::Dumper::Indent = 0;
+		$self->_logger->debug( "State @{[ $self->session_state ]}: Events " . Dumper([ sort { $a cmp $b } @$events ]) )
+	} if $self->_logger->is_debug;
+	my @events_cb = @{ $self->_event_dispatch_table }{ @$events };
+
+	my $transition_event = await Future->needs_any( map {
+		my $idx = $_;
+		my $f = $events_cb[$idx]->($self)
+			->transform( done => sub { $events->[$idx] } )
+			->set_label( $events->[$idx] )
+	} 0..@events_cb-1 )->set_label("$self : events");
+
+	my $from = $self->session_state;
+	my $transition_data = $self->state_machine->process_event(
+		$self,
+		$transition_event
+	);
+
+	do {
+		$self->_logger->debug( "Transition: [ @{[ $from ]} ] -- @{[ $transition_event ]} --> [ @{[ $transition_data->{to} ]} ]" )
+	} if $self->_logger->is_debug;
+
+	$self->session_state( $transition_data->{to} );
+
+	my @actions = @{ $transition_data->{action} };
+	my $actions_done = Future->needs_all( map {
+		my $action = $_;
+		my $f = $self->_action_dispatch_table->{$action}->($self)
+			->set_label($action)
+	} @actions)->set_label( 'actions' );
+
+	await $actions_done->followed_by( sub { $self->_reset_after_step; Future->done } );
+
+	return {
+		from => $from,
+		transition => $transition_event,
+		to => $transition_data->{to}
+	};
+}
+
+sub _reset_after_step { }
 
 1;
