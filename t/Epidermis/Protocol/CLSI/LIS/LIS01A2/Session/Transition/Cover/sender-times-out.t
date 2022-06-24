21d3959f7f1c70bd2e8f90d08e96fae69776019c
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-06-24 18:55:12 -0400

    Use Import::Into to simplify test file

--- t/Epidermis/Protocol/CLSI/LIS/LIS01A2/Session/Transition/Cover/sender-times-out.t
+++ t/Epidermis/Protocol/CLSI/LIS/LIS01A2/Session/Transition/Cover/sender-times-out.t
@@ -1,6 +1,5 @@
 #!/usr/bin/env perl
 use lib 't/lib';
-use Test2::V0;
 use Test::SessionSim::FromDataFileRel;
 Test::SessionSim::FromDataFileRel->run_tests;
 done_testing;
--- t/lib/Test/SessionSim/FromDataFile.pm
+++ t/lib/Test/SessionSim/FromDataFile.pm
@@ -1,6 +1,7 @@
 package Test::SessionSim::FromDataFile;
 # ABSTRACT: Read data file to load testing steps
 
+use Test2::V0;
 use Test2::Roo;
 use MooX::ShortHas;
 use Path::Tiny;
@@ -10,6 +11,13 @@ use Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::Constants
 use Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::Driver::Commands;
 
 ro 'data_path';
+
+use Import::Into;
+
+sub import {
+	my $caller = scalar caller;
+	Test2::V0->import::into($caller);
+}
 
 lazy _data => sub {
 	do "". path($_[0]->data_path)->absolute;
