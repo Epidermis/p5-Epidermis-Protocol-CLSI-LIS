4ccebc3f3bc186d0052dea347526ed7ea50861dd
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-06-24 15:32:24 -0400

    Turn state machine coverage into data-driven tests

---
+++ t/Epidermis/Protocol/CLSI/LIS/LIS01A2/Session/Transition/Cover/sender-times-out.t
@@ -0,0 +1,6 @@
+#!/usr/bin/env perl
+use lib 't/lib';
+use Test2::V0;
+use Test::SessionSim::FromDataFileRel;
+Test::SessionSim::FromDataFileRel->run_tests;
+done_testing;
---
+++ t/lib/Test/SessionSim/FromDataFile.pm
@@ -0,0 +1,27 @@
+package Test::SessionSim::FromDataFile;
+# ABSTRACT: Read data file to load testing steps
+
+use Test2::Roo;
+use MooX::ShortHas;
+use Path::Tiny;
+
+use Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::Constants
+	qw(:enum_state :enum_event);
+use Epidermis::Protocol::CLSI::LIS::LIS01A2::Session::Driver::Commands;
+
+ro 'data_path';
+
+lazy _data => sub {
+	do "". path($_[0]->data_path)->absolute;
+};
+
+lazy local_steps => sub { $_[0]->_data->{local}; };
+lazy remote_steps => sub { $_[0]->_data->{remote}; };
+
+before setup => sub {
+	note "Testing steps: ", $_[0]->data_path;
+};
+
+with 'Test::SessionSim';
+
+1;
